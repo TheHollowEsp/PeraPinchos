0e0950d1dcc27c4725b29ab4aa419981502b4eb0
TheHollowEsp <hrlopez@esei.uvigo.es>
2014-11-30 17:33:32 +0100

    Configurada conexion a la DB

--- src/core/PDOConnection.php
+++ src/core/PDOConnection.php
@@ -3,9 +3,9 @@
 
 class PDOConnection {
   private static $dbhost = "127.0.0.1";
-  private static $dbname = "mvcblog";
-  private static $dbuser = "mvcuser";
-  private static $dbpass = "mvcblogpass";
+  private static $dbname = "perapinchos";
+  private static $dbuser = "root";
+  private static $dbpass = "pera";
   private static $db_singleton = null;
   
   public static function getInstance() {
