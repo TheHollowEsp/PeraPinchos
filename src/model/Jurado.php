d33ee14aecbc16ae522306acb57c937081d03fb9
peter <peter-puessi@hotmail.com>
2014-12-01 00:44:53 +0100

    modelado de Jurado
    creadas jurado y juradoMapper(posiblemente incompleta)

---
+++ src/model/Jurado.php
@@ -0,0 +1,64 @@
+<?php
+// file: model/Jurado.php
+
+require_once (__DIR__ . "/../core/ValidationException.php");
+
+class JuradoBean {
+	var $dniJurado;
+	var $nombreJurado;
+	var $isJuradoP;
+	
+	public function __construct(dniJurado=NULL, nombreJurado=NULL, isJuradoP=NULL) {
+		$this -> dniJurado = $dniJurado;
+		$this -> nombreJurado = $nombreJurado;
+		$this -> isJuradoP = $isJuradoP;
+	}
+	
+	public function getDniJurado() {
+		return $this->dniJurado;
+	}
+	
+	public function setDniJurado($dniJurado) {
+		$this->dniJurado = $dniJurado;
+	}
+	
+	public function getNombreJurado() {
+		return $this->nombreJurado;
+	}
+	
+	public function setNombreJurado($nombreJurado) {
+		$this->nombreJurado = $nombreJurado;
+	}
+
+	public function getIsProfesional() {
+		return $this->isJuradoP;
+	}
+	
+	public function setIsProfesional($isJuradoP) {
+		$this->isJuradoP = $isJuradoP;
+	}
+
+	public function __toString() {
+		return "DNI: ".$this->dniJurado.
+			 ", Nombre: ".$this->nombreJurado.
+			 ", Profesional: ".$this->isJuradoP;
+	}
+	
+	//revisa si el elemento jurado a troducir es correcto
+	public function checkIsValidForRegisterJurado() {
+		$errors = array();
+		if (strlen(trim($this -> dniJurado)) == 0) {
+			$errors["dniJurado"] = "El DNI es obligatorio";
+			if (strlen($this->dniJurado) < 9) {
+				$errors["dniJurado"] = "El DNI introducido tiene una longitud invalida";
+			}
+		}
+		if (strlen(trim($this -> nombreJurado)) == 0) {
+			$errors["nombreJurado"] = "El Nombre del Jurado es obligatorio";
+		}
+		if (sizeof($errors)>0){
+			throw new ValidationException($errors, "Jurado no valido");
+		}
+  } 
+}
+?>
---
+++ src/model/JuradoMapper.php
@@ -0,0 +1,67 @@
+<?php
+// file: model/JuradoMapper.php
+
+require_once(__DIR__."/../core/PDOConnection.php");
+
+class JuradoMapper {
+
+  private $db;
+  
+  public function __construct() {
+    $this->db = PDOConnection::getInstance();
+  }
+   
+  public function save(Jurado $jurado) {
+    $stmt = $this->db->prepare("INSERT INTO Jurado values (?,?,?)");
+    $stmt->execute(array(
+	$jurado->getDniJurado(), 
+	$jurado->getNombreJurado(), 
+	$jurado->getIsProfesional));  
+  }
+  
+  	public function findAll() {
+		$stmt = $this -> db -> query("SELECT * FROM jurado");
+		$jurado_db = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+		$juradoL = array();
+
+		foreach ($jurado_db as $jurado) {			
+			$jurado1 = new Jurado($jurado["Establecimiento_NombreEst"]);
+			array_push($juradoL, new Jurado($jurado["dniJurado"], $jurado["nombreJurado"], $jurado["isJuradoP"]));
+		}
+		return $juradoL;
+	}
+  
+  public function juradoExistsByName(Jurado $nombreJurado) {
+    $stmt = $this->db->prepare("SELECT count(nombreJurado) FROM Jurado where nombreJurado=?");
+    $stmt->execute(array($nombreJurado));
+    
+    if ($stmt->fetchColumn() > 0) {   
+      return true;
+    } 
+  }
+  
+  public function juradoExistsByDNI($dniJurado) {
+    $stmt = $this->db->prepare("SELECT count(dniJurado) FROM Jurado where dniJurado=?");
+    $stmt->execute(array($dniJurado));
+    
+    if ($stmt->fetchColumn() > 0) {   
+      return true;
+    } 
+  }
+  
+  public function isValidJurado($dniJurado, $nombreJurado) {
+    $stmt = $this->db->prepare("SELECT count(dniJurado) FROM Jurado where dniJurado=? and nombreJurado=?");
+    $stmt->execute(array($dniJurado, $nombreJurado));
+    
+    if ($stmt->fetchColumn() > 0) {
+      return true;        
+    }
+  }
+  
+  	public function delete(Jurado $jurado) {
+		$stmt = $this -> db -> prepare("DELETE from jurado WHERE dniJurado=?");
+		$stmt -> execute(array($jurado -> getDniJurado()));
+	}
+	
+}
