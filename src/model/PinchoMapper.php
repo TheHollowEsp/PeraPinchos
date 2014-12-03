dfd7a4218e1fd0e475ccf6e71a1c3c6b4841b31f
TheHollowEsp <hrlopez@esei.uvigo.es>
2014-12-03 14:39:37 +0100

    Añadida funcion base para conectar pincho y jurado

--- src/model/PinchoMapper.php
+++ src/model/PinchoMapper.php
@@ -6,7 +6,6 @@ require_once (__DIR__ . "/../model/Establecimiento.php");
 
 require_once (__DIR__ . "/../model/Pincho.php");
 
-
 ini_set('display_errors', 'On');
 /**
  * Class PinchoMapper
@@ -28,12 +27,41 @@ class PinchoMapper {
 		$this -> db = PDOConnection::getInstance();
 	}
 
-	/**
-	 * Devuelve todos los pinchos
-	 *
-	 * @throws PDOException if a database error occurs
-	 * @return mixed Array of Post instances (without comments)
-	 */
+	//FIXME: Le falta poner atributos de jurado
+	public function findAllForJuradoP() {
+		$stmt = query("SELECT * FROM pincho NATURAL JOIN pincho_has_jurado NATURAL JOIN jurado");
+		$pinchos_db = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+		$pinchos = array();
+		foreach ($pinchos_db as $pincho) {
+			//$establecimiento = new Establecimiento($pincho["Establecimiento_NombreEst"]);
+			//Usar nombres de las columnas en la DB
+			array_push($pinchos, new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"], $pincho["Fotos"], $pincho["Informacion"], $pincho["Validado"], $pincho["Establecimiento_NombreEst"]));
+		}
+
+		return $pinchos;
+
+	}
+
+	//FIXME: Por probar, falta poner atributos de jurado en array
+	public function findAllForJuradoConcreto($jurado) {
+		$stmt = $this -> db -> prepare("SELECT * FROM pincho NATURAL JOIN pincho_has_jurado NATURAL JOIN jurado WHERE jurado.DNIJur=?");
+
+		$stmt -> execute(array($jurado -> getDniJurado()));
+
+		$pinchos_db = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+		$pinchos = array();
+		foreach ($pinchos_db as $pincho) {
+			//$establecimiento = new Establecimiento($pincho["Establecimiento_NombreEst"]);
+			//Usar nombres de las columnas en la DB
+			array_push($pinchos, new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"], $pincho["Fotos"], $pincho["Informacion"], $pincho["Validado"], $pincho["Establecimiento_NombreEst"]));
+		}
+
+		return $pinchos;
+
+	}
+
 	public function findAll() {
 		$stmt = $this -> db -> query("SELECT * FROM pincho");
 		$pinchos_db = $stmt -> fetchAll(PDO::FETCH_ASSOC);
@@ -41,9 +69,9 @@ class PinchoMapper {
 		$pinchos = array();
 
 		foreach ($pinchos_db as $pincho) {
-			$establecimiento = new Establecimiento($pincho["Establecimiento_NombreEst"]);
+			//$establecimiento = new Establecimiento($pincho["Establecimiento_NombreEst"]);
 			//Usar nombres de las columnas en la DB
-			array_push($pinchos, new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"],$pincho["Fotos"], $pincho["Informacion"],$pincho["Validado"], $pincho["Establecimiento_NombreEst"]));
+			array_push($pinchos, new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"], $pincho["Fotos"], $pincho["Informacion"], $pincho["Validado"], $pincho["Establecimiento_NombreEst"]));
 		}
 
 		return $pinchos;
@@ -63,7 +91,7 @@ class PinchoMapper {
 		$pincho = $stmt -> fetch(PDO::FETCH_ASSOC);
 
 		if (!sizeof($pincho) == 0) {
-			return new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"],$pincho["Fotos"], $pincho["Informacion"],$pincho["Validado"], $pincho["Establecimiento_NombreEst"]);
+			return new Pincho($pincho["NombrePincho"], $pincho["Descripcion"], $pincho["Precio"], $pincho["Ingredientes"], $pincho["Fotos"], $pincho["Informacion"], $pincho["Validado"], $pincho["Establecimiento_NombreEst"]);
 		} else {
 			return NULL;
 		}
@@ -76,15 +104,7 @@ class PinchoMapper {
 	 */
 	public function save(Pincho $pincho) {
 		$stmt = $this -> db -> prepare("INSERT INTO pincho (NombrePincho, Descripcion, Precio, Ingredientes, Fotos, Informacion, Validado, Establecimiento_NombreEst) (?,?,?,?,?,?,?,?)");
-		$stmt -> execute(array(
-		$pincho -> getNombrePincho(),
-		$pincho -> getDescripcionPincho(),
-		$pincho -> getPrecioPincho(),
-		$pincho -> getIngredientesPincho(),
-		$pincho -> getFotosPincho(),
-		$pincho -> getInfoPincho(),
-		$pincho -> getIsValidado(),
-		$pincho -> getEstablecimiento() -> getNombre() //Revisar
+		$stmt -> execute(array($pincho -> getNombrePincho(), $pincho -> getDescripcionPincho(), $pincho -> getPrecioPincho(), $pincho -> getIngredientesPincho(), $pincho -> getFotosPincho(), $pincho -> getInfoPincho(), $pincho -> getIsValidado(), $pincho -> getEstablecimiento() -> getNombre() //Revisar
 		));
 	}
 
@@ -95,16 +115,8 @@ class PinchoMapper {
 	 */
 	public function update(Pincho $pincho) {
 		$stmt = $this -> db -> prepare("UPDATE pincho set Descripcion=?, Precio=?, Ingredientes=?, Fotos=?, Informacion=?, Validado=? where NombrePincho=?");
-		$stmt -> execute(array(
-		$pincho -> getDescripcionPincho(),
-		$pincho -> getPrecioPincho(),
-		$pincho -> getIngredientesPincho(),
-		$pincho -> getFotosPincho(),
-		$pincho -> getInfoPincho(),
-		$pincho -> getIsValidado(),
-		$pincho -> getNombrePincho()
-		));
-	} //Aquí no se cambia la clave foranea
+		$stmt -> execute(array($pincho -> getDescripcionPincho(), $pincho -> getPrecioPincho(), $pincho -> getIngredientesPincho(), $pincho -> getFotosPincho(), $pincho -> getInfoPincho(), $pincho -> getIsValidado(), $pincho -> getNombrePincho()));
+	}//Aquí no se cambia la clave foranea
 
 	/**
 	 * Borra el pincho de la base de datos
