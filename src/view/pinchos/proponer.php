43ea105441fc087b1bee880f1dbbe4869c3f3e9c
TheHollowEsp <hrlopez@esei.uvigo.es>
2014-12-28 22:56:04 +0100

    Corrección de errores

--- src/view/pinchos/proponer.php
+++ src/view/pinchos/proponer.php
@@ -8,55 +8,44 @@ $errors = $view -> getVariable("errors");
 $pincho = $view -> getVariable("pincho");
 $view -> setVariable("title", "Proponer pincho");
 ?>
-<h1><?= i18n("Propon un pincho") ?></h1>
-<form action="index.php?controller=pinchos&amp;action=proponer" method="POST">
-	
-      	
-       
-	<div class="form-group">
-      <label for="inputNombre"> <?= i18n("Nombre") ?>:</label> 
-      <input id="inputNombre" class="form-control" type="text" name="nombre" value="">
-      <?= isset($errors["nombre"]) ? $errors["nombre"] : "" ?><br>
-      </div>
-      <div class="form-group">
-      <label for="inputDesc">  <?= i18n("Descripcion") ?>: </label> 
-      	<input id="inputDesc" class="form-control"  type="text" name="descripcion" value="">
-      <?= isset($errors["descripcion"]) ? $errors["descripcion"] : "" ?><br>
-       </div>
-      <div class="form-group">
-      <div class="input-group">
-      	<label for="inputPrecio"> <?= i18n("Precio") ?>:</label> 
-  	
-  <input id="inputPrecio" class="form-control" type="number" name="precio" value="" class="form-control" aria-label="Amount (to the nearest euro)">
-
-</div>
-
-      
-      <?= isset($errors["precio"]) ? $errors["precio"] : "" ?><br>
+<html>
+	<h1><?= i18n("Propon un pincho") ?></h1>
+		<form action="index.php?controller=pinchos&amp;action=proponer" method="POST">       
+			<div class="form-group">
+		      	<label for="inputNombre"> <?= i18n("Nombre") ?>:</label> 
+		      	<input id="inputNombre" class="form-control" type="text" name="nombre" value="">
+		      	<?= isset($errors["nombre"]) ? $errors["nombre"] : "" ?><br>
+      		</div>
+			<div class="form-group">
+		      	<label for="inputDesc">  <?= i18n("Descripcion") ?>: </label> 
+		      	<input id="inputDesc" class="form-control"  type="text" name="descripcion" value="">
+		      	<?= isset($errors["descripcion"]) ? $errors["descripcion"] : "" ?><br>
+		    </div>
+      		<div class="form-group">
+      			<div class="input-group">
+      				<label for="inputPrecio"> <?= i18n("Precio") ?>:</label>   	
+  					<input id="inputPrecio" class="form-control" type="number" name="precio" value="" class="form-control">
+			</div>      
+      		<?= isset($errors["precio"]) ? $errors["precio"] : "" ?><br>
       </div>
       
       <div class="form-group">
-      <label for="inputNombre"> <?= i18n("Ingredientes") ?>:</label> 
-      <input id="inputNombre" class="form-control"type="text" name="ingredientes" 
-			value="">
-      <?= isset($errors["ingredientes"]) ? $errors["ingredientes"] : "" ?><br>
+      	<label for="inputNombre"> <?= i18n("Ingredientes") ?>:</label> 
+      	<input id="inputNombre" class="form-control"type="text" name="ingredientes" value="">
+      	<?= isset($errors["ingredientes"]) ? $errors["ingredientes"] : "" ?><br>
       </div>
       
       <div class="form-group">
-    <label for="einputFile"> <?= i18n("Foto") ?>:</label>
-    <input type="file" id="einputFile" type="file" name="foto" enctype="multipart/form-data">
-      <?= isset($errors["foto"]) ? $errors["foto"] : "" ?><br>
+    	<label for="einputFile"> <?= i18n("Foto") ?>:</label>
+   		<input type="file" id="einputFile" type="file" name="foto" enctype="multipart/form-data">
+      	<?= isset($errors["foto"]) ? $errors["foto"] : "" ?><br>
       </div>
       
       <div class="form-group">
-      <label for="inputInfo">  <?= i18n("Informacion adicional") ?>:</label> 
-      <input id="inputInfo" class="form-control"type="text" name="info" 
-			value="">
-      <?= isset($errors["info"]) ? $errors["info"] : "" ?><br>
-            
-      
-      
-      
-      <input class="btn btn-default btn-lg" type="submit" value="<?= i18n("Proponer") ?>"></div>
-
-</form>
+      	<label for="inputInfo">  <?= i18n("Informacion adicional") ?>:</label> 
+      	<input id="inputInfo" class="form-control"type="text" name="info"	value="">
+      	<?= isset($errors["info"]) ? $errors["info"] : "" ?><br>      
+      	<input class="btn btn-default btn-lg" type="submit" value="<?= i18n("Proponer") ?>">
+      </div>
+	</form>
+</html>
